02e377157fa402dec54ccdbf04cbe6448a6449ea
Robert Romero <robert02frontend@gmail.com>
2025-03-05 18:37:53 -0500

    add new english

--- src/api/openai.js
+++ src/api/openai.js
@@ -8,22 +8,22 @@ const getChatAI = async (prompt) => {
   const systemprompt = `
   Asiste como asistente de estudio proporcionando ayuda con tareas e investigaciones en inglés, así como en otros temas según se requiera.
 
-Asegúrate de ofrecer explicaciones claras y detalladas para mejorar la comprensión del estudiante. Responde a preguntas específicas, ofrece consejos sobre técnicas de estudio, y proporciona recursos útiles cuando sea necesario.
+    Asegúrate de ofrecer explicaciones claras y detalladas para mejorar la comprensión del estudiante. Responde a preguntas específicas, ofrece consejos sobre técnicas de estudio, y proporciona recursos útiles cuando sea necesario.
 
-# Steps
+    # Steps
 
-1. Recibir la pregunta o tema de estudio del usuario.
-2. Identificar el aspecto específico en el que se necesita ayuda.
-3. Buscar y proporcionar información relevante, explicaciones o recursos que puedan ayudar al usuario.
-4. Ofrecer sugerencias de técnicas de estudio o métodos de investigación según el tema.
-5. Asegurarse de que el estudiante comprenda claramente la información proporcionada y realizar aclaraciones adicionales si es necesario.
+    1. Recibir la pregunta o tema de estudio del usuario.
+    2. Identificar el aspecto específico en el que se necesita ayuda.
+    3. Buscar y proporcionar información relevante, explicaciones o recursos que puedan ayudar al usuario.
+    4. Ofrecer sugerencias de técnicas de estudio o métodos de investigación según el tema.
+    5. Asegurarse de que el estudiante comprenda claramente la información proporcionada y realizar aclaraciones adicionales si es necesario.
 
- # Json Schema
-  {
-    "title": "title",
-    "response":
-    "response"
-  }
+  # Json Schema
+    {
+      "title": "title",
+      "response":
+      "response"
+    }
 `;
 
   const response = await axios.post(
@@ -66,8 +66,11 @@ Asegúrate de ofrecer explicaciones claras y detalladas para mejorar la comprens
     }
   );
 
+  console.log(response, "all response");
+
   const jsonResponse = JSON.parse(response.data.choices[0].message.content);
-  console.log(jsonResponse, "response hola");
+  console.log(jsonResponse, "response json");
+
   return jsonResponse;
 };
 
---
+++ src/app/english/page.js
@@ -0,0 +1,127 @@
+"use client";
+import React, { useState } from "react";
+import getEnglishChat from "./api/english";
+import Markdown from "markdown-to-jsx";
+
+export default function Page() {
+  const [input, setInput] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [textresponse, setTextResponse] = useState("");
+
+  const handleSend = async () => {
+    setIsLoaded(true);
+    const response = await getEnglishChat(input);
+    setTextResponse(response);
+    setIsLoaded(false);
+  };
+
+  return (
+    <main className="flex flex-col min-h-screen bg-background w-full md:mx-auto pt-[10em] relative px-4">
+      page
+      <div
+        className="flex flex-col items-center justify-center w-[600px] min-h-[40vh] mx-auto py-10
+      border border-gray-400 rounded-md
+      bg-white"
+      >
+        <h1>My English</h1>
+        <div className="pb-2 pt-10 px-10">
+          {textresponse.response && (
+            <MarkDownComponent response={textresponse.response} />
+          )}
+        </div>
+        <textarea
+          type="text"
+          className="border border-gray-300 rounded-md p-2 mb-4 w-[90%]"
+          placeholder="Enter text"
+          value={input}
+          min={2}
+          max={10}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              !isLoaded && handleSend();
+            }
+          }}
+        />
+        <button className="bg-blue-500 text-white rounded-md p-2  w-[90%]">
+          Submit
+        </button>
+      </div>
+    </main>
+  );
+}
+
+const MarkDownComponent = ({ response }) => {
+  const MyParagraph = ({ children, ...props }) => (
+    <div {...props}>{children}</div>
+  );
+  const H1Component = ({ children, ...props }) => (
+    <h1 {...props}>{children}</h1>
+  );
+  const H2Component = ({ children, ...props }) => (
+    <h2 {...props}>{children}</h2>
+  );
+  const H3Component = ({ children, ...props }) => (
+    <h3 {...props}>{children}</h3>
+  );
+  const H4Component = ({ children, ...props }) => (
+    <h4 {...props}>{children}</h4>
+  );
+  const PComponent = ({ children, ...props }) => <p {...props}>{children}</p>;
+  return (
+    <Markdown
+      className="text-lg"
+      options={{
+        overrides: {
+          h1: {
+            component: H1Component,
+            props: {
+              className: "text-3xl font-semibold border-b text-gray-700",
+            },
+          },
+          h2: {
+            component: H2Component,
+            props: {
+              className: "text-2xl font-semibold underline text-gray-700",
+            },
+          },
+          h3: {
+            component: H3Component,
+            props: {
+              className:
+                "text-xl md:text-2xl font-semibold py-4  md:py-6 underline text-gray-700",
+            },
+          },
+          h4: {
+            component: H4Component,
+            props: {
+              className:
+                "text-lg md:text-2xl font-semibold py-4  md:py-6 text-gray-700",
+            },
+          },
+          p: {
+            component: PComponent,
+            props: {
+              className: "text-md font-normal py-2 text-gray-700",
+            },
+          },
+          strong: {
+            component: "strong",
+            props: {
+              className: "font-semibold text-gray-70",
+            },
+          },
+          ul: {
+            component: MyParagraph,
+            props: {
+              className:
+                "text-[16px] md:text-xl px-2 md:px-6 list-disc list-inside",
+            },
+          },
+        },
+      }}
+    >
+      {response}
+    </Markdown>
+  );
+};
