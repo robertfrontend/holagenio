3fd2288f22485291f342f744fb6179077e05affb
Robert Romero <robert02frontend@gmail.com>
2025-03-13 07:50:46 -0400

    refactor: english page

--- src/app/hub/english/page.js
+++ src/app/hub/english/page.js
@@ -1,12 +1,12 @@
 "use client";
 import React, { useState } from "react";
-import GetEnglishAPI from "./api/index";
 import Markdown from "markdown-to-jsx";
 import MainCard from "@/components/hub/MainCard";
 import { Send } from "lucide-react";
 import useRateLimiter from "@/app/hooks/userRateLimiter";
 import ModalSuscription from "@/components/hub/ModalSuscription";
 import HubChat from "@/components/hub/HubChat";
+import { GetComplementationAPI } from "@/api/openai";
 
 export default function Page() {
   const [input, setInput] = useState("");
@@ -29,7 +29,22 @@ export default function Page() {
     incrementRequestCount();
 
     setIsLoaded(true);
-    const response = await GetEnglishAPI(value);
+
+    const systemprompt = `Eres un asistente especializado 
+  en inglés que tiene como objetivo ayudar
+  al usuario a aprender y practicar el idioma.
+  Tu rol es apoyar al usuario con explicaciones
+  claras y simples. Siempre responde en inglés
+  sencillo y fácil de entender.
+  Si el usuario comete errores, corrígelos
+  suavemente y explica brevemente por qué está mal.
+   Además, proporciona ejemplos claros y cortos para m
+   ejorar la comprensión. Enfócate en las necesidades
+   específicas del usuario, ya sea vocabulario, gramática,
+    pronunciación o conversación.`;
+
+    const response = await GetComplementationAPI(value, [], systemprompt);
+
     setTextResponse(response);
     setIsLoaded(false);
   };
